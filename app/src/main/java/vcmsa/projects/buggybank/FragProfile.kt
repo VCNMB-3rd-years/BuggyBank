5465fdf3671827b7b4fc352759cc54e06716709f
Aphiwe Mhotwana <127925737+ST10085670@users.noreply.github.com>
2025-06-08 23:16:34 +0200

    Sign in and up have been update dramatically and the profile page is done

--- app/src/main/java/vcmsa/projects/buggybank/FragProfile.kt
+++ app/src/main/java/vcmsa/projects/buggybank/FragProfile.kt
@@ -1,59 +1,212 @@
 package vcmsa.projects.buggybank
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FragProfile.newInstance] factory method to
- * create an instance of this fragment.
- */
+
+
 class FragProfile : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+    private var nameTextInputLayout: TextInputLayout? = null
+    private var emailTextInputLayout: TextInputLayout? = null
+    private var ageTextInputLayout: TextInputLayout? = null
+    private var phoneTextInputLayout: TextInputLayout? = null
+    private var surnameTextInputLayout: TextInputLayout? = null
+    private var addressTextInputLayout: TextInputLayout? = null
+    private var btnSave: Button? = null
+    private var imagePreview: ShapeableImageView? = null
+    private var profileImageUri: Uri? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_frag_profile, container, false)
+        nameTextInputLayout = view.findViewById(R.id.txtName)
+        emailTextInputLayout = view.findViewById(R.id.txtProfileEmail)
+        ageTextInputLayout = view.findViewById(R.id.txtBirthday)
+        phoneTextInputLayout = view.findViewById(R.id.txtPhone)
+        surnameTextInputLayout = view.findViewById(R.id.txtSurname)
+        addressTextInputLayout = view.findViewById(R.id.txtAddress)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val uid = FirebaseAuth.getInstance().currentUser?.uid
+        imagePreview = view.findViewById(R.id.imgProfile)
+        imagePreview?.setOnClickListener {
+            val options = arrayOf("Camera", "Gallery")
+            androidx.appcompat.app.AlertDialog.Builder(requireContext())
+                .setTitle("Select Image")
+                .setItems(options) { _, which ->
+                    when (which) {
+                        0 -> {
+                            Log.d("FragProfile", "Camera selected")
+                            requestCameraPermission()
+                        }
+                        1 -> {
+                            Log.d("FragProfile", "Gallery selected")
+                            requestGalleryPermission()
+                        }
+                    }
+                }
+                .show()
+        }
+        btnSave = view.findViewById(R.id.btnSave)
+        btnSave?.setOnClickListener {
+            val name = nameTextInputLayout?.editText?.text.toString()
+            val email = emailTextInputLayout?.editText?.text.toString()
+            val age = ageTextInputLayout?.editText?.text.toString()
+            val phone = phoneTextInputLayout?.editText?.text.toString()
+            val surname = surnameTextInputLayout?.editText?.text.toString()
+            val address = addressTextInputLayout?.editText?.text.toString()
+            
+            Log.d("FragProfile", "Save button clicked")
+            Log.d("FragProfile", "Name: $name")
+            Log.d("FragProfile", "Email: $email")
+            Log.d("FragProfile", "Age: $age")
+            Log.d("FragProfile", "Phone: $phone")
+            Log.d("FragProfile", "Surname: $surname")
+            Log.d("FragProfile", "Address: $address")
+            Log.d("FragProfile", "Profile image URI: $profileImageUri")
+            updateDetails(name, email, age, phone, surname, address, profileImageUri)
+        }
+        if (uid != null) {
+            FirebaseDatabase.getInstance().getReference("users").child(uid).child("details")
+                .addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            val name = dataSnapshot.child("username").value as String
+                            val email = dataSnapshot.child("email").value as String
+                            val age = dataSnapshot.child("age").value as String
+                            val phone = dataSnapshot.child("phone").value as String
+                            val surname = dataSnapshot.child("surname").value as String
+                            val address = dataSnapshot.child("address").value as String
+
+                            phoneTextInputLayout?.editText?.setText(phone)
+                            surnameTextInputLayout?.editText?.setText(surname)
+                            addressTextInputLayout?.editText?.setText(address)
+                            nameTextInputLayout?.editText?.setText(name)
+                            emailTextInputLayout?.editText?.setText(email)
+                            ageTextInputLayout?.editText?.setText(age)
+                        } else {
+                            phoneTextInputLayout?.editText?.setText("")
+                            surnameTextInputLayout?.editText?.setText("")
+                            addressTextInputLayout?.editText?.setText("")
+                            nameTextInputLayout?.editText?.setText("")
+                            emailTextInputLayout?.editText?.setText("")
+                            ageTextInputLayout?.editText?.setText("")
+                        }
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        Log.e("Error", error.message)
+                    }
+                })
+        }
+    }
+
+    private fun updateDetails(name: String, email: String, age: String, phone: String, surname: String, address: String, profileImageUri: Uri? = null) {
+        val uid = FirebaseAuth.getInstance().currentUser?.uid
+        if (uid != null) {
+            FirebaseDatabase.getInstance().getReference("users").child(uid).child("details")
+                .child("username").setValue(name)
+            FirebaseDatabase.getInstance().getReference("users").child(uid).child("details")
+                .child("email").setValue(email)
+            FirebaseDatabase.getInstance().getReference("users").child(uid).child("details")
+                .child("age").setValue(age)
+            FirebaseDatabase.getInstance().getReference("users").child(uid).child("details")
+                .child("phone").setValue(phone)
+            FirebaseDatabase.getInstance().getReference("users").child(uid).child("details")
+                .child("surname").setValue(surname)
+            FirebaseDatabase.getInstance().getReference("users").child(uid).child("details")
+                .child("address").setValue(address)
+
+            if (profileImageUri != null) {
+                val storageReference = FirebaseStorage.getInstance().reference.child("images/$uid")
+                storageReference.putFile(profileImageUri)
+                    .addOnSuccessListener {
+                        Log.d("FragProfile", "Image uploaded to Firebase Storage")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.e("FragProfile", "Error uploading image to Firebase Storage", e)
+                    }
+            }
         }
     }
     
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frag_profile, container, false)
+
+    private fun requestCameraPermission() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.CAMERA),
+                101
+            )
+        } else {
+            takePictureWithCamera()
+        }
     }
-    
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragProfile.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragProfile().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    private fun requestGalleryPermission() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                102
+            )
+        } else {
+            selectImageFromGallery()
+        }
+    }
+
+    private fun takePictureWithCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, 100)
+    }
+
+    private fun selectImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        startActivityForResult(intent, 101)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 100 && resultCode == Activity.RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            profileImageUri = data?.data
+            imagePreview?.setImageBitmap(imageBitmap)
+        } else if (requestCode == 101 && resultCode == Activity.RESULT_OK) {
+            profileImageUri = data?.data
+            imagePreview?.setImageURI(profileImageUri)
+        }
     }
 }
--- app/src/main/java/vcmsa/projects/buggybank/Sign_in.kt
+++ app/src/main/java/vcmsa/projects/buggybank/Sign_in.kt
@@ -1,3 +1,4 @@
+// Sign_in.kt
 package vcmsa.projects.buggybank
 
 import android.content.Intent
@@ -6,20 +7,11 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.credentials.Credential
-import androidx.credentials.CustomCredential
-import androidx.credentials.GetCredentialRequest
 import androidx.lifecycle.lifecycleScope
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.android.libraries.identity.googleid.GetGoogleIdOption
-import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
-import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential.Companion.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL
 import com.google.firebase.FirebaseNetworkException
 import com.google.firebase.auth.*
 import com.google.firebase.auth.ktx.auth
@@ -37,11 +29,6 @@ class Sign_in : AppCompatActivity() {
     
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivitySignInBinding
-    private lateinit var googleSignInClient: GoogleSignInClient
-    
-    companion object {
-        private const val RC_SIGN_IN = 9001
-    }
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,29 +44,14 @@ class Sign_in : AppCompatActivity() {
             insets
         }
         
-        val googleIdOption = GetGoogleIdOption.Builder()
-            .setServerClientId(getString(R.string.default_web_client_id))
-            .setFilterByAuthorizedAccounts(true)
-            .build()
-        
-        val request = GetCredentialRequest.Builder()
-            .addCredentialOption(googleIdOption)
-            .build()
-        
-        googleSignInClient = GoogleSignIn.getClient(this, GoogleSignInOptions.DEFAULT_SIGN_IN)
-        
         onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
-                val intent = Intent(this@Sign_in, Sign_up::class.java)
-                startActivity(intent)
-                overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+                startActivity(Intent(this@Sign_in, Sign_up::class.java))
                 finish()
             }
         })
         
-        binding.SignInButton.setOnClickListener {
-            signInUser()
-        }
+        binding.SignInButton.setOnClickListener { signInUser() }
         
         binding.SignInRegister.setOnClickListener {
             startActivity(Intent(this, Sign_up::class.java))
@@ -90,30 +62,33 @@ class Sign_in : AppCompatActivity() {
             startActivity(Intent(this, ForgotPasswordActivity::class.java))
             finish()
         }
-        
-        binding.btnGoogle.setOnClickListener {
-            val signInIntent = googleSignInClient.signInIntent
-            startActivityForResult(signInIntent, RC_SIGN_IN)
-        }
     }
     
     private fun signInUser() {
+        val email = binding.SignInEmail.text.toString().trim()
+        val password = binding.SignInPassword.text.toString().trim()
+        
         lifecycleScope.launch(Dispatchers.IO) {
             try {
-                val email = binding.SignInEmail.text.toString().trim()
-                val password = binding.SignInPassword.text.toString().trim()
-                auth.signInWithEmailAndPassword(email, password).await()
+                val result = auth.signInWithEmailAndPassword(email, password).await()
+                val user = result.user ?: throw Exception("User is null")
                 
-                val userId = auth.currentUser?.uid ?: throw Exception("User ID is null after sign-in")
-                val dbRef = FirebaseDatabase.getInstance().getReference("users").child(userId)
-                dbRef.child("signedIn").setValue(true).await()
+                if (!user.isEmailVerified) {
+                    withContext(Dispatchers.Main) {
+                        showVerificationDialog(user)
+                    }
+                    return@launch
+                }
+                
+                FirebaseDatabase.getInstance().getReference("users").child(user.uid)
+                    .child("signedIn").setValue(true).await()
                 
                 withContext(Dispatchers.Main) {
                     startActivity(Intent(this@Sign_in, MenuBar::class.java))
                     finish()
                 }
+                
             } catch (e: Exception) {
-                Log.e(TAG, "signInUser: Login failed", e)
                 withContext(Dispatchers.Main) {
                     handleAuthException(e)
                 }
@@ -121,42 +96,29 @@ class Sign_in : AppCompatActivity() {
         }
     }
     
-    
-    private fun firebaseAuthWithGoogle(idToken: String) {
-        val credential = GoogleAuthProvider.getCredential(idToken, null)
-        lifecycleScope.launch(Dispatchers.IO) {
-            try {
-                auth.signInWithCredential(credential).await()
-                
-                val userId = auth.currentUser?.uid ?: throw Exception("User ID is null after Google sign-in")
-                val dbRef = FirebaseDatabase.getInstance().getReference("users").child(userId)
-                dbRef.child("signedIn").setValue(true).await()
-                
-                withContext(Dispatchers.Main) {
-                    startActivity(Intent(this@Sign_in, MenuBar::class.java))
-                    finish()
-                }
-            } catch (e: Exception) {
-                Log.e(TAG, "firebaseAuthWithGoogle: Google sign-in failed", e)
-                withContext(Dispatchers.Main) {
-                    Toast.makeText(this@Sign_in, "Google Sign-In failed", Toast.LENGTH_LONG).show()
-                }
-            }
-        }
-    }
-    
-    override fun onStart() {
-        super.onStart()
-        val currentUser = auth.currentUser ?: return
-        val dbRef = FirebaseDatabase.getInstance().getReference("users")
-            .child(currentUser.uid).child("signedIn")
+    private fun showVerificationDialog(user: FirebaseUser) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Email Not Verified")
+        builder.setMessage("Please verify your email before logging in.")
         
-        dbRef.get().addOnCompleteListener { task ->
-            if (task.isSuccessful && task.result.exists() && task.result.getValue(Boolean::class.java) == true) {
-                startActivity(Intent(this, MenuBar::class.java))
-                finish()
+        builder.setPositiveButton("Resend Email") { _, _ ->
+            user.sendEmailVerification().addOnCompleteListener { task ->
+                Toast.makeText(
+                    this,
+                    if (task.isSuccessful) "Verification email resent." else "Failed to send email.",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
+        
+        builder.setNeutralButton("Change Email") { _, _ ->
+            auth.signOut()
+            startActivity(Intent(this, Sign_up::class.java))
+            finish()
+        }
+        
+        builder.setNegativeButton("Cancel", null)
+        builder.show()
     }
     
     private fun handleAuthException(e: Exception) {
@@ -169,18 +131,5 @@ class Sign_in : AppCompatActivity() {
             else -> "Authentication failed: ${e.localizedMessage}"
         }
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-    }
-    
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == RC_SIGN_IN) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                val account = task.getResult(ApiException::class.java)!!
-                firebaseAuthWithGoogle(account.idToken!!)
-            } catch (e: ApiException) {
-                Log.w(TAG, "Google sign in failed", e)
-            }
-        }
     }
 }
